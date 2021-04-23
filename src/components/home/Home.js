4ea3b27010f30ce9f8f0e4a2208e41dd882e52f0
taron.grigoryan <ggrigoryantaron@gmail.com>
2021-04-23 14:12:59 +0400

    create half cycle

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,15 +15,15 @@ const Home = () => {
                 <div className={'hero-title-holder'}>
                     <div className={'overlay'}>
                         <div className={'hero-title'}>
-                            <div className={'hero-decor b-dec'}>
-                                <div className={'half-circle'}></div>
-                            </div>
+
                             <div className={'text-content'}>
                                 <h3>Discover and book tables</h3>
                                 <span className={'border-span'}></span> <h4>IN YEREVAN RESTAURANTS</h4> <span
                                 className={'border-span'}></span>
                             </div>
-
+                            <div className={'hero-decor b-dec'}>
+                                <div className={'half-circle'}></div>
+                            </div>
                             <div className={'btn-book'}>
                                 <Link to={'/restaurants'}><Button context={'Book Now'}/> </Link>
                             </div>
